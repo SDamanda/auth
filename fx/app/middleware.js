82af29e0699be7993b6f9c2052bdd7d552b8e072
Amanda dos Santos Daros <amandsan08@gmail.com>
2025-09-15 13:11:05 -0300

    push

--- fx/app/middleware.js
+++ fx/app/middleware.js
@@ -1,35 +1,30 @@
 import { NextResponse, NextRequest } from "next/server";
 
-const rotaspublicas = [
-    { path: "/cadastro", whenAuthenticated: "redirect" },
-    { path: "/register", whenAuthenticated: "redirect" },
-    { path: "/pricing", whenAuthenticated: "next" } // pública para todos
-];
+const publicrouter = [
+        { path: "/cadastro", whenAuthenticated: "redirect" },
+        { path: "/login", whenAuthenticated: "redirect" },
+    ];
 
-const redirectIfNotAuthenticated = "/sign-in";
+const redirectIfNotAuthenticated = "/cadastro";
 
 export function middleware(NextRequest) {
     const { pathname } = request.nextUrl;
 
-    // Verifica se a rota é pública
-    const foundRoute = rotaspublicas.find(route => route.path === pathname);
+    const foundRoute = publicrouter
+        .find(route => route.path === pathname);server
 
-    // Pega o token do cookie (aqui só checa existência, não valida)
     const token = request.cookies.get("token")?.value;
 
-    // Se rota pública e usuário não autenticado, deixa acessar
     if (foundRoute && !token) {
         return NextResponse.next();
     }
 
-    // Se rota privada (não está em rotaspublicas) e usuário não autenticado, redireciona para login
     if (!foundRoute && !token) {
         const url = request.nextUrl.clone();
         url.pathname = redirectIfNotAuthenticated;
         return NextResponse.redirect(url);
     }
 
-    // Se rota pública e o usuário está autenticado e a rota pede redirecionar
     if (foundRoute && token && foundRoute.whenAuthenticated === "redirect") {
         const url = request.nextUrl.clone();
         url.pathname = "/cadastro"; // redireciona ao dashboard
--- fx/app/publicas/cadastro/page.js
+++ fx/app/publicas/cadastro/page.js
@@ -8,9 +8,9 @@ export default function Cadastro() {
     const [senha, setSenha] = useState('')
     const [numero, setNumero] = useState('')
 
-    const supabase = createClient(
-        process.env.,NEXT_PUBLIC_SUPABASE_URL
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    process.env.NEXT_PUBLIC_SUPABASE_URL
     );
     async function signUp (nome, email, senha, numero){
         const {data, error} = await supabase.auth.signUp({
@@ -19,7 +19,6 @@ export default function Cadastro() {
         if (error ) return error.message;
         return data;
     }
-
 
     return (
         <>
